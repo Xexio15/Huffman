7fbc3bab8ac960857fcd88c6548f48b88129e18b
xexio15 <xexio15@gmail.com>
2019-04-04 17:02:16 +0200

    Added menu

--- src/ub/huffman/Main.java
+++ src/ub/huffman/Main.java
@@ -23,23 +23,44 @@ public class Main {
 
         //String text = "KDQKKD9D10JDDQKDJQ1010JKDJQDJDJJQQDQKQKJK10QKQKDQQDDKDK910JKDDQQ10KKDQQDJDKJQ10DKK10DJDDKDKDQ10JDQD109DJ99JQDD"; //Demo.txt
         //String text = "DD9Q9DDD910KD10KQDDQQD10DDD10DJKDDKDDDKDDDDJDKDDKQDDDKDKKDDJ9DDDDDQKDDDQKDQDDDDDDQJKKKDKDKD10JQDDDDK9DDKK"; //Demo2.txt
+        boolean cont = true;
 
+        while (cont) {
+            System.out.println("\n1- Random message");
+            System.out.println("2- Custom message");
+            System.out.println("3- Exit");
+            int option = sc.nextInt();
+            String text = "";
+            if(option == 1) {
+                System.out.println("Enter message size:");
+                int len = sc.nextInt();
+                text = Utils.randomMessage(t.table, len);
+            }
 
-        System.out.println("Enter message size:");
-        int len = sc.nextInt();
-        String text = Utils.randomMessage(t.table,len);
-        System.out.println("\nText: "+text);
-        String encoded = t.encode(text);
-        System.out.println("Encoded Text: "+encoded);
+            else if (option == 2){
+                System.out.println("Enter message");
+                text = sc.next();
+            }
 
-        String decoded = t.decode(encoded);
-        System.out.println("Decoded Text: "+decoded+"\n");
+            else{
+                cont = false;
+            }
 
-        assert text.equals(decoded);
+            if (cont) {
+                System.out.println("\nText: " + text);
+                String encoded = t.encode(text);
+                System.out.println("Encoded Text: " + encoded);
 
-        System.out.println("Average used bits: " + t.averageBits()+ " bits");
-        System.out.println("Enthropy (Theoric minimum): " + t.enthropy()+ " bits");
-        System.out.println("Needed bits without compression: "+ t.getNumBits());
-        System.out.println("Compression Ratio: " + t.compressionRatio());
+                String decoded = t.decode(encoded);
+                System.out.println("Decoded Text: " + decoded + "\n");
+
+                assert text.equals(decoded);
+
+                System.out.println("Average used bits: " + t.averageBits() + " bits");
+                System.out.println("Enthropy (Theoric minimum): " + t.enthropy() + " bits");
+                System.out.println("Needed bits without compression: " + t.getNumBits());
+                System.out.println("Compression Ratio: " + t.compressionRatio());
+            }
+        }
     }
 }
